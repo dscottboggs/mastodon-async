0f32f74272c34c545f779855f441f245fc5ec9a1
D. Scott Boggs <scott@tams.tech>
2023-01-09 08:25:10 -0500

    Add List ID type

--- entities/src/list.rs
+++ entities/src/list.rs
@@ -3,6 +3,23 @@ use serde::{Deserialize, Serialize};
 /// Used for ser/de of list resources
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
 pub struct List {
-    id: String,
+    id: ListId,
     title: String,
+}
+
+/// Wrapper type for a list ID string
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[serde(transparent)]
+pub struct ListId(String);
+
+impl AsRef<str> for ListId {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+impl ListId {
+    pub fn new(value: impl Into<String>) -> Self {
+        Self(value.into())
+    }
 }
