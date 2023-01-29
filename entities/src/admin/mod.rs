d8d340c1febeca45d9d245ef781e8d0e41d58dc8
D. Scott Boggs <scott@tams.tech>
2023-01-29 10:39:21 -0500

    Add entity for dealing with blocked email domains

--- entities/src/admin/mod.rs
+++ entities/src/admin/mod.rs
@@ -3,9 +3,9 @@ pub mod canonical_email_block;
 pub mod cohort;
 pub mod dimension;
 pub mod domain;
+pub mod email_domain_block;
 
 pub use account::*;
 pub use canonical_email_block::*;
 pub use cohort::{Cohort, CohortFrequency};
 pub use dimension::Dimension;
-pub use domain_allow::DomainAllow;
