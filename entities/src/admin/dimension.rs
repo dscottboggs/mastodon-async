722342ca6965f5bc86a6e84209b29088de5daa84
D. Scott Boggs <scott@tams.tech>
2023-02-12 12:59:13 -0500

    Add Admin::Dimension test example

--- entities/src/admin/dimension.rs
+++ entities/src/admin/dimension.rs
@@ -27,3 +27,46 @@ pub struct Data {
     /// A human-readable formatted value for this data item.
     pub human_value: Option<String>,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_space_usage_example() {
+        let example = r#"{
+        	"key": "space_usage",
+        	"data": [
+        		{
+        			"key": "postgresql",
+        			"human_key": "PostgreSQL",
+        			"value": "14924935",
+        			"unit": "bytes",
+        			"human_value": "14.2 MB"
+        		},
+        		{
+        			"key": "redis",
+        			"human_key": "Redis",
+        			"value": "1972544",
+        			"unit": "bytes",
+        			"human_value": "1.88 MB"
+        		},
+        		{
+        			"key": "media",
+        			"human_key": "Media storage",
+        			"value": "0",
+        			"unit": "bytes",
+        			"human_value": "0 Bytes"
+        		}
+        	]
+        }"#;
+        let subject: Dimension = serde_json::from_str(example).unwrap();
+        assert_eq!(subject.key, DimensionKey::new("space_usage"));
+        let data = &subject.data[0];
+        assert_eq!(data.key, DimensionDataKey::new("postgresql"));
+        assert_eq!(data.human_key, "PostgreSQL");
+        assert_eq!(data.value, "14924935");
+        assert_eq!(data.unit.as_ref().unwrap(), "bytes");
+        assert_eq!(data.human_value.as_ref().unwrap(), "14.2 MB");
+    }
+}
