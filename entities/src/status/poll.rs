ff1850a3b83f2b47db05f924e479aa63e3a7be32
D. Scott Boggs <scott@tams.tech>
2023-02-12 11:57:34 -0500

    Add Poll example test

--- entities/src/status/poll.rs
+++ entities/src/status/poll.rs
@@ -2,16 +2,16 @@ use derive_builder::Builder;
 use serde::{Deserialize, Serialize};
 use time::{serde::iso8601, OffsetDateTime};
 
-use crate::prelude::CustomEmoji;
+use crate::{prelude::CustomEmoji, PollId};
 
 /// Represents a poll attached to a status.
 ///
 /// See also [the API documentation](https://docs.joinmastodon.org/entities/Poll/)
-#[derive(Debug, Builder, Default, Clone, Serialize, Deserialize, PartialEq, Eq)]
+#[derive(Debug, Builder, Clone, Serialize, Deserialize, PartialEq, Eq)]
 #[builder(build_fn(error = "crate::error::Error"))]
 pub struct Poll {
     /// The ID of the poll in the database.
-    pub id: String,
+    pub id: PollId,
     /// When the poll ends.
     #[serde(with = "iso8601::option")]
     pub expires_at: Option<OffsetDateTime>,
@@ -33,7 +33,7 @@ pub struct Poll {
     /// When called with a user token, which options has the authorized user
     /// chosen? Contains an array of index values for options.
     #[serde(default)]
-    pub own_votes: Vec<u16>,
+    pub own_votes: Vec<usize>,
 }
 
 #[derive(Debug, Builder, Default, Clone, Serialize, Deserialize, PartialEq, Eq)]
@@ -43,4 +43,52 @@ pub struct PollOption {
     /// The total number of received votes for this option. `None` if the
     /// results aren't published yet.
     pub votes_count: Option<u64>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_name() {
+        let example = r#"{
+          "id": "34830",
+          "expires_at": "2019-12-05T04:05:08.302Z",
+          "expired": true,
+          "multiple": false,
+          "votes_count": 10,
+          "voters_count": null,
+          "voted": true,
+          "own_votes": [
+            1
+          ],
+          "options": [
+            {
+              "title": "accept",
+              "votes_count": 6
+            },
+            {
+              "title": "deny",
+              "votes_count": 4
+            }
+          ],
+          "emojis": []
+        }"#;
+        let poll: Poll = serde_json::from_str(example).unwrap();
+        assert_eq!(poll.id, PollId::new("34830"));
+        assert!(poll.expired);
+        assert!(!poll.multiple);
+        assert_eq!(poll.votes_count, 10);
+        assert!(poll.voters_count.is_none());
+        assert!(poll.voted.is_some());
+        assert!(poll.voted.unwrap());
+        assert_eq!(poll.own_votes, [1]);
+        let option = &poll.options[0];
+        assert_eq!(option.title, "accept");
+        assert_eq!(option.votes_count, Some(6));
+        let option = &poll.options[1];
+        assert_eq!(option.title, "deny");
+        assert_eq!(option.votes_count, Some(4));
+        assert!(poll.emojis.is_empty());
+    }
 }
