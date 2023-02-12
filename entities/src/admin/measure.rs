dcc7e3c11bf2760dbae6709ebf375285a011b23e
D. Scott Boggs <scott@tams.tech>
2023-02-12 12:30:11 -0500

    Add Admin::Measure example test

--- entities/src/admin/measure.rs
+++ entities/src/admin/measure.rs
@@ -14,13 +14,14 @@ pub struct Measure {
     #[serde(with = "conversion::string_to_u64")]
     pub total: u64,
     /// A human-readable formatted value for this data item.
-    pub human_value: String,
+    #[serde(default)]
+    pub human_value: Option<String>,
     /// The numeric total associated with the requested measure, in the previous
     /// period. Previous period is calculated by subtracting the start_at and
     /// end_at dates, then offsetting both start and end dates backwards by the
     /// length of the time period.
-    #[serde(with = "conversion::string_to_u64")]
-    pub previous_total: u64,
+    #[serde(with = "conversion::string_to_u64::option")]
+    pub previous_total: Option<u64>,
     /// The data available for the requested measure, split into daily buckets.
     pub data: Vec<Data>,
 }
@@ -34,4 +35,67 @@ pub struct Data {
     /// The numeric value for the requested measure.
     #[serde(with = "conversion::string_to_u64")]
     pub value: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    use time::Month;
+
+    use super::*;
+
+    #[test]
+    fn test_measure_example() {
+        let example = r#"{
+          "key": "active_users",
+          "unit": null,
+          "total": "2",
+          "previous_total": "0",
+          "data": [
+            {
+              "date": "2022-09-14T00:00:00Z",
+              "value": "0"
+            },
+            {
+              "date": "2022-09-15T00:00:00Z",
+              "value": "0"
+            },
+            {
+              "date": "2022-09-16T00:00:00Z",
+              "value": "0"
+            },
+            {
+              "date": "2022-09-17T00:00:00Z",
+              "value": "1"
+            },
+            {
+              "date": "2022-09-18T00:00:00Z",
+              "value": "1"
+            },
+            {
+              "date": "2022-09-19T00:00:00Z",
+              "value": "1"
+            },
+            {
+              "date": "2022-09-20T00:00:00Z",
+              "value": "2"
+            },
+            {
+              "date": "2022-09-21T00:00:00Z",
+              "value": "1"
+            }
+          ]
+        }"#;
+        let subject: Measure = serde_json::from_str(example).unwrap();
+        assert_eq!(subject.key, MeasureKey::new("active_users"));
+        assert!(subject.unit.is_none());
+        assert_eq!(subject.total, 2);
+        assert_eq!(subject.previous_total, Some(0));
+        assert!(subject.human_value.is_none());
+        let data = &subject.data[0];
+        assert_eq!(data.value, 0);
+        let date = data.date.date();
+        assert_eq!(date.year(), 2022);
+        assert_eq!(date.month(), Month::September);
+        assert_eq!(date.day(), 14);
+    }
 }
