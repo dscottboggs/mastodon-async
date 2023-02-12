a063aec468bb00be04daaac7a8d16d426f15f38f
D. Scott Boggs <scott@tams.tech>
2023-02-12 13:11:21 -0500

    Add Admin::Cohort example test; change conversion namespace

--- entities/src/admin/cohort.rs
+++ entities/src/admin/cohort.rs
@@ -1,5 +1,7 @@
+use is_variant::IsVariant;
 use serde::{Deserialize, Serialize};
 use time::{serde::iso8601, OffsetDateTime};
+use crate::conversion;
 
 /// Represents a retention metric.
 ///
@@ -18,21 +20,11 @@ pub struct Cohort {
 /// The size of the bucket for the returned [`Cohort`] data.
 ///
 /// See also [the API documentation](https://docs.joinmastodon.org/entities/Admin_Cohort/#frequency)
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, IsVariant)]
+#[serde(rename_all = "lowercase")]
 pub enum CohortFrequency {
     Day,
     Month,
-}
-
-impl CohortFrequency {
-    #![allow(missing_docs)]
-
-    pub fn is_day(&self) -> bool {
-        *self == Self::Day
-    }
-    pub fn is_month(&self) -> bool {
-        *self == Self::Month
-    }
 }
 
 /// Represents a single value from a set of retention metrics.
@@ -47,5 +39,31 @@ pub struct Data {
     /// were active for the given date bucket.
     pub rate: f64,
     /// How many users registered in the specified period and were active for the given date bucket.
+    #[serde(with = "conversion::string_to::i64")]
     pub value: i64,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cohort_example() {
+        let example = r#"{
+        	"period": "2022-09-01T00:00:00+00:00",
+        	"frequency": "month",
+        	"data": [
+        		{
+        			"date": "2022-09-01T00:00:00+00:00",
+        			"rate": 1.0,
+        			"value": "2"
+        		}
+        	]
+        }"#;
+        let subject: Cohort = serde_json::from_str(example).unwrap();
+        assert!(subject.frequency.is_month());
+        let data = &subject.data[0];
+        assert_eq!(data.rate, 1.0);
+        assert_eq!(data.value, 2);
+    }
 }
