349a8b92961e20fcee8fead3e33e8d002517c24b
D. Scott Boggs <scott@tams.tech>
2023-03-23 07:42:24 -0400

    more nits

--- entities/src/account.rs
+++ entities/src/account.rs
@@ -518,7 +518,7 @@ pub struct Suggestion {
     account: Account,
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, is_enum_variant)]
+#[derive(Debug, Clone, Deserialize, Serialize, PartialEq, Eq, is_enum_variant)]
 #[serde(rename_all = "snake_case")]
 pub enum SuggestionSource {
     Staff,
