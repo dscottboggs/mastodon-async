8639c71689951ec0af422f3bfb82c607b59b1353
D. Scott Boggs <scott@tams.tech>
2023-06-04 11:46:03 -0400

    Add implementation for From to ids

--- entities/src/ids.rs
+++ entities/src/ids.rs
@@ -33,6 +33,12 @@ macro_rules! define_ids {
                 write!(f, "{}", self.0)
             }
         }
+        
+        impl From<$from_t> for $name {
+            fn from(it: $from_t) -> $name {
+                $name(it)
+            }
+        }
     };
     () => {}
 }
