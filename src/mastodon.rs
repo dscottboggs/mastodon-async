34828b20fe9da9b4a8d2de99b71871fcd26b0c38
D. Scott Boggs <scott@tams.tech>
2024-04-10 15:28:15 -0400

    Add GET keyword by ID method

--- src/mastodon.rs
+++ src/mastodon.rs
@@ -128,6 +128,7 @@ impl Mastodon {
         (put<-forms::filter::Update) update_filter[FilterId]: "filters/{}" => Filter,
         (get) get_filter_keywords[FilterId]: "filters/{}/keywords" => Vec<filter::Keyword>,
         (post<-forms::filter::add::Keyword) add_keyword_to_filter[FilterId]: "filters/{}/keywords" => filter::Keyword,
+        (get) get_filter_keyword[KeywordId]: "filters/keywords/{}" => filter::Keyword,
     }
 
     streaming! {
